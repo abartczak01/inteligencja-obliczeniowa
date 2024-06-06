65172a90cc61c7da6af23e406aae3d07fad2ea8c
abartczak629 <a.bartczak.629@studms.ug.edu.pl>
2024-06-06 09:01:55 +0200

    lab08

--- lab08/zad03/zad03.py
+++ lab08/zad03/zad03.py
@@ -1,19 +1,7 @@
 import numpy as np
 import pygad
-
-# Reprezentacja labiryntu jako dwuwymiarowej macierzy
-# labirynt = np.array([
-#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#     [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-#     [1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-#     [1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-#     [1, 0, 1, 0, 0, 0, 1, 0, 1, 1],
-#     [1, 0, 1, 1, 1, 1, 1, 0, 0, 1],
-#     [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-#     [1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-# ])
+import matplotlib.pyplot as plt
+import time
 
 labirynt = np.array([
     [1,1,1,1,1,1,1,1,1,1,1,1],
@@ -34,9 +22,13 @@ labirynt = np.array([
 def fitness_func(model, solution, solution_idx):
     x, y = 1, 1  # Początkowe położenie w labiryncie
     penalty = 0
+    award = 0
 
-    for move in solution:
-        if move == 0 and labirynt[x-1, y] != 1:  # w górę
+    for i, move in enumerate(solution):
+        if x == 10 and y == 10:
+            award = 5/(i + 1)  # nagroda za dojście do konća, im mniej kroków (mniejsze i) tym większa nagroda
+            break
+        elif move == 0 and labirynt[x-1, y] != 1:  # w górę
             x -= 1
         elif move == 1 and labirynt[x+1, y] != 1:  # w dół
             x += 1
@@ -45,41 +37,77 @@ def fitness_func(model, solution, solution_idx):
         elif move == 3 and labirynt[x, y+1] != 1:  # w prawo
             y += 1
         else:
-            penalty += 1  # Ruch na pole ze ścianką lub wyjście poza labirynt
+            penalty = (abs(10 - x) + abs(10 - y)) * 0.25 # im blżej początku trasy błąd tym wieksza trasa
+            break
 
     distance_to_exit = abs(10 - x) + abs(10 - y)
     
     if x < 0 or x >= labirynt.shape[0] or y < 0 or y >= labirynt.shape[1]:
         return 0  # Bardzo niska wartość fitness za wyjście poza labirynt
-
-    fitness = 1 / (distance_to_exit + 1)  # Bliskość do wyjścia
-    fitness -= penalty * 0.2  # Kara za ruchy na ścianę lub poza labirynt
+    
+    fitness = 1 / (distance_to_exit + 1) - penalty + award
 
     return fitness
 
-# Parametry algorytmu genetycznego
-num_generations = 500
-sol_per_pop = 50
+# def on_stop(model, fitness_vals):
+#     print(fitness_vals)
+
+
+num_generations = 5000
+sol_per_pop = 40
 num_genes = 30
-num_parents_mating = 8
+num_parents_mating = 10
+keep_parents = 2
 gene_space = [0, 1, 2, 3]  #  góra, dół, lewo, prawo
-mutation_percent_genes = 0.15
+mutation_percent_genes = 7 # 2 zmutowane geny na 30
 
-# Inicjacja algorytmu genetycznego
+times = []
+for i in range(10):
+    print(i, "próba")
+    ga_instance = pygad.GA(gene_space=gene_space,
+                        num_generations=num_generations,
+                        sol_per_pop=sol_per_pop,
+                        num_genes=num_genes,
+                        mutation_percent_genes=mutation_percent_genes,
+                        num_parents_mating=num_parents_mating,
+                        keep_parents=keep_parents,
+                        fitness_func=fitness_func,
+                        stop_criteria="reach_1",
+                        #    on_stop=on_stop
+                        #    save_best_solutions=True
+                        )
+    start = time.time()
+    ga_instance.run()
+    end = time.time()
+    times.append(end - start)
+
+print("wszystkie czasy:")
+for i in times:
+    print(f"{i:.2f} s")
+print(f"średni czas: {sum(times)/10:.2f}")
+
+
 ga_instance = pygad.GA(gene_space=gene_space,
-                       num_generations=num_generations,
-                       sol_per_pop=sol_per_pop,
-                       num_genes=num_genes,
-                       num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_func)
+                        num_generations=1000,
+                        sol_per_pop=sol_per_pop,
+                        num_genes=num_genes,
+                        mutation_percent_genes=mutation_percent_genes,
+                        num_parents_mating=num_parents_mating,
+                        keep_parents=keep_parents,
+                        fitness_func=fitness_func,
+                        # stop_criteria="reach_1",
+                        #    on_stop=on_stop
+                        #    save_best_solutions=True
+                        )
 
-# Uruchomienie algorytmu genetycznego
+start = time.time()
 ga_instance.run()
+end = time.time()
 
-# Wyświetlenie najlepszego rozwiązania
+
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
 print("Najlepsze rozwiązanie:", solution)
-print("Wartość funkcji fitness dla najlepszego rozwiązania:", solution_fitness, (1-solution_fitness)/solution_fitness)
+print("Wartość funkcji fitness dla najlepszego rozwiązania:", solution_fitness)
 directions_mapping = {
     0: "góra",
     1: "dół",
@@ -87,18 +115,20 @@ directions_mapping = {
     3: "prawo"
 }
 
-# Konwertowanie rozwiązania na słowa
+# konwertowanie rozwiązania na słowa
 solution_words = [directions_mapping[direction] for direction in solution]
 
-# Wydrukowanie rozwiązania
 print("Najlepsze rozwiązanie:", solution_words)
 
 def simulate_solution(labirynt, solution):
-    x, y = 1, 1  # Początkowe położenie w labiryncie
-    path = [(x, y)]  # Śledzenie ścieżki
+    x, y = 1, 1
+    path = [(x, y)]
 
     for move in solution:
-        if move == 0 and labirynt[x-1, y] != 1:  # w górę
+        if (x, y) == (10, 10):
+            print("Sukces! Dotarliśmy do wyjścia.")
+            return path
+        elif move == 0 and labirynt[x-1, y] != 1:  # w górę
             x -= 1
         elif move == 1 and labirynt[x+1, y] != 1:  # w dół
             x += 1
@@ -107,7 +137,7 @@ def simulate_solution(labirynt, solution):
         elif move == 3 and labirynt[x, y+1] != 1:  # w prawo
             y += 1
         else:
-            print(f"Nielegalny ruch: ({x}, {y}) przy ruchu {move}")
+            print(f"Nielegalny ruch: ({x}, {y}) przy ruchu {directions_mapping[move]}")
             break  # Nielegalny ruch
         path.append((x, y))
     
@@ -120,4 +150,22 @@ def simulate_solution(labirynt, solution):
 
 # Przeprowadzenie symulacji
 path = simulate_solution(labirynt, solution)
-print("Ścieżka:", path)
+print("Ścieżka:", path, ", liczba kroków ", len(path)-1)
+ga_instance.plot_fitness()
+plt.savefig('fitness_plot.png')
+print("=============== czas", end - start)
+
+# dopytać się o stop_criteria, nie zapisuje najlepszego rozwiązania tylko najlepsze rozwiązanie z generacji wcześniejszej
+
+# wszystkie czasy:
+# 3.59 s
+# 0.27 s
+# 0.20 s
+# 0.52 s
+# 0.37 s
+# 0.64 s
+# 1.84 s
+# 0.30 s
+# 0.29 s
+# 0.22 s
+# średni czas: 0.82
